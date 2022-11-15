a59879f5355e348568106137ce1fa7b17f88a3d0
TosinHub <tosinolugbenga@gmail.com>
2022-11-15 11:44:55 +0000

    test written with jest

---
+++ app.test.js
@@ -0,0 +1,47 @@
+const request = require("supertest");
+const app = require("./app");
+
+describe("CREDIT CARD SYSTEM", () => {
+  it("POST  /cards ---> created cards ", () => {
+    return request(app)
+      .post("/todos")
+      .send({
+        name: "Oluwatosin Gbenga",
+        card_number: 4353342343425,
+        limit: 2000
+      })
+      .expect("Content-Type", /json/)
+      .expect(201)
+      .then((response) => {
+        expect(response.body).toEqual(
+          expect.arrayContaining([
+            expect.objectContaining({
+              name: "Oluwatosin Gbenga",
+              card_number: 4353342343425,
+              limit: 2000
+            }),
+          ])
+        );
+      });
+  });
+
+
+
+  it("GET /cards --> array of cards ", () => {
+    return request(app)
+      .get("/cards")
+      .expect("Content-Type", /json/)
+      .expect(200)
+      .then((response) => {
+        expect(response.body).toEqual(
+          expect.arrayContaining([
+            expect.objectContaining({
+              name: expect.any(String),
+              card_number: expect.any(Number),
+              limit: expect.any(Number),
+            }),
+          ])
+        );
+      });
+  });
+});
